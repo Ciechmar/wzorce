1c1c469fb8a60f66d0a3a272851a5dc3ebcf889f
Ciechmar <Ciechmar@o2.pl>
2020-06-01 18:59:07 +0200

    2. Dodanie wzorzec Observator - Observer

--- src/main/java/observerZadanie/App.java
+++ src/main/java/observerZadanie/App.java
@@ -3,12 +3,13 @@ package observerZadanie;
 public class App {
     public static void main(String[] args) {
         Button button = new Button();
-        SmsSender smsSender = new SmsSender();
-        SmsSender smsSender2 = new SmsSender();
-        EmailSender emailSender = new EmailSender();
-        button.addLisneter(smsSender);
-        button.addLisneter(smsSender2);
-        button.addLisneter(emailSender);
+//        SmsSender smsSender = new SmsSender();
+//        SmsSender smsSender2 = new SmsSender();
+//        EmailSender emailSender = new EmailSender();
+        button.addLisneter(new SmsSender());
+        button.addLisneter(new SmsSender());
+        button.addLisneter(new EmailSender());
+        button.addLisneter(new EmailSender());
         button.click();
 
 
